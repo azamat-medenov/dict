09e7acb9d44d22a3438566747c50ef1d5b847be6
Azamat <medenovvaza@gmail.com>
2023-08-17 18:37:21 +0600

    created Entry model

--- dictionary/words/models.py
+++ dictionary/words/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    word = models.CharField(max_length=255, unique=True)
+    translation = models.CharField(max_length=255)
+    context = models.TextField()
